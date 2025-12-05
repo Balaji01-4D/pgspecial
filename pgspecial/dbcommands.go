619d053c6da87fb055645bbe8fe2bfcdfd08d285
Balaji01-4D <j.balaji2468@gmail.com>
2025-12-05 17:39:14 +0530

    feat: add ListDefaultPrivileges

--- pgspecial/dbcommands.go
+++ pgspecial/dbcommands.go
@@ -2,6 +2,7 @@ package pgspecial
 
 import (
 	"context"
+	"fmt"
 	"strconv"
 	"strings"
 )
@@ -293,5 +294,47 @@ func ListRoles(ctx context.Context, db DB, pattern string, verbose bool) (*Resul
 		Status:  "OKAY",
 	}
 	
+	return res, nil
+}
+
+
+func ListDefaultPrivileges(ctx context.Context, db DB, pattern string, verbose bool) (*Result, error) {
+	var sb strings.Builder
+	args := []any{}
+
+	sb.WriteString(`
+	 SELECT pg_catalog.pg_get_userbyid(d.defaclrole) AS owner,
+    n.nspname AS schema,
+    CASE d.defaclobjtype WHEN 'r' THEN 'table'
+                         WHEN 'S' THEN 'sequence'
+                         WHEN 'f' THEN 'function'
+                         WHEN 'T' THEN 'type'
+                         WHEN 'n' THEN 'schema' END as type,
+    pg_catalog.array_to_string(d.defaclacl, E'\n') AS access_privileges
+    FROM pg_catalog.pg_default_acl d
+        LEFT JOIN pg_catalog.pg_namespace n ON n.oid = d.defaclnamespace
+	`)
+	if pattern != "" {
+		sb.WriteString(`
+		 WHERE (n.nspname OPERATOR(pg_catalog.~) $1 COLLATE pg_catalog.default
+            OR pg_catalog.pg_get_userbyid(d.defaclrole) OPERATOR(pg_catalog.~) $1 COLLATE pg_catalog.default)
+		`)
+		args = append(args, fmt.Sprintf("^(%s)$", pattern))
+	}
+	sb.WriteString("ORDER BY 1, 2, 3;")
+
+	fmt.Println(sb.String())
+
+	rows, err := db.Query(ctx, sb.String(), args...)
+	if err != nil {
+		return nil, err
+	}
+
+	res := &Result{
+		Title:   "Default Privileges",
+		Rows:    rows,
+		Columns: rows.FieldDescriptions(),
+		Status:  "OKAY",
+	}
 	return res, nil
 }
