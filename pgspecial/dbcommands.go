e8925a5c7fa8ca09e42b860f09e1207e2be98d7d
Balaji01-4D <j.balaji2468@gmail.com>
2025-12-05 22:15:58 +0530

    feat: add ListTableSpaces

--- pgspecial/dbcommands.go
+++ pgspecial/dbcommands.go
@@ -338,3 +338,59 @@ func ListDefaultPrivileges(ctx context.Context, db DB, pattern string, verbose b
 	}
 	return res, nil
 }
+
+func ListTablespaces(ctx context.Context, db DB, pattern string, verbose bool) (*Result, error) {
+	var sb strings.Builder
+	args := []any{}
+	argIndex := 1
+
+
+	var isLocationSupported bool
+	rows := db.QueryRow(ctx, `
+	  SELECT EXISTS (
+            SELECT * FROM pg_proc
+            WHERE proname = 'pg_tablespace_location'
+        )
+	`)
+	if err := rows.Scan(&isLocationSupported); err != nil {
+		return nil, err
+	}
+
+	sb.WriteString(`
+	SELECT
+		n.spcname AS name,
+		pg_catalog.pg_get_userbyid(n.spcowner) AS owner,
+	`)
+	if isLocationSupported {
+		sb.WriteString("    pg_catalog.pg_tablespace_location(n.oid) AS location\n")
+	} else {
+		sb.WriteString("    'Not supported' AS location\n")
+	}
+
+	sb.WriteString(`
+	FROM pg_catalog.pg_tablespace n
+	`)
+
+	if pattern != "" {
+		_, tablePattern := sqlNamePattern(pattern)
+		if tablePattern != "" {
+			sb.WriteString(" WHERE n.spcname ~ $" + strconv.Itoa(argIndex) + " COLLATE pg_catalog.default ")
+			args = append(args, tablePattern)
+		}
+	}
+
+	sb.WriteString(" ORDER BY 1;")
+	rowsResult, err := db.Query(ctx, sb.String(), args...)
+	if err != nil {
+		return nil, err
+	}
+
+	res := &Result{
+		Title:   "TABLESPACES",
+		Rows:    rowsResult,
+		Columns: rowsResult.FieldDescriptions(),
+		Status:  "OKAY",
+	}
+	return res, nil	
+
+}
