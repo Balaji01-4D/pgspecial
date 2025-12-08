fd6990d84ce81d4cc944e39c695a23b45599219f
Balaji01-4D <j.balaji2468@gmail.com>
2025-12-08 09:39:49 +0530

    refactor: add dummy verbose

--- dbcommands/shell_command_test.go
+++ dbcommands/shell_command_test.go
@@ -10,7 +10,7 @@ import (
 func TestShellCommand_Success(t *testing.T) {
 	ctx := context.Background()
 
-	_, err := dbcommands.ShellCommand(ctx, nil, "echo hello")
+	_, err := dbcommands.ShellCommand(ctx, nil, "echo hello", false)
 	if err != nil {
 		t.Fatalf("expected no error, got %v", err)
 	}
@@ -20,7 +20,7 @@ func TestShellCommand_Failure(t *testing.T) {
 	ctx := context.Background()
 
 	// invalid command should error
-	_, err := dbcommands.ShellCommand(ctx, nil, "commandthatdoesnotexist")
+	_, err := dbcommands.ShellCommand(ctx, nil, "commandthatdoesnotexist", false)
 	if err == nil {
 		t.Fatalf("expected error, got nil")
 	}
