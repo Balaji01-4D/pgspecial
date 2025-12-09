b76529d49c725f2b9feed6d000bca59af082b295
Balaji01-4D <j.balaji2468@gmail.com>
2025-12-09 22:36:52 +0530

    docs: add docs to queryer

--- database/queryer.go
+++ database/queryer.go
@@ -1,3 +1,4 @@
+// this package provide the interface to interact with the database
 package database
 
 import (
@@ -6,6 +7,9 @@ import (
 	"github.com/jackc/pgx/v5"
 )
 
+// Queryer is an interface that defines methods for querying a database.
+// It includes methods for executing queries that return multiple rows, single rows, and closing the connection.
+// test with pgx.Conn and pgxpool.Pool
 type Queryer interface {
 	Query(ctx context.Context, sql string, args ...any) (pgx.Rows, error)
 	QueryRow(ctx context.Context, sql string, args ...any) pgx.Row
