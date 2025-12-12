396f55afe07cb72a9134af5e83e40f7f50ae8174
Balaji01-4D <j.balaji2468@gmail.com>
2025-12-12 23:05:53 +0530

    updated types

--- types.go
+++ types.go
@@ -18,10 +18,51 @@ type SpecialCommand struct {
 // It defines the command name, optional aliases, documentation metadata, and
 // execution handler used when registering commands via RegisterCommand.
 type SpecialCommandRegistry struct {
-	Cmd          string
+	Cmd           string
 	Alias         []string
 	Syntax        string
 	Description   string
 	Handler       SpecialHandler
 	CaseSensitive bool
+}
+
+type TableFooterMeta struct {
+	Indexes          []string // lines under "Indexes:"
+	CheckConstraints []string // "Check constraints:"
+	ForeignKeys      []string // "Foreign-key constraints:"
+	ReferencedBy     []string // "Referenced by:"
+    ViewDefinition   *string  // "View definition:"
+
+	RulesEnabled  []string // under "Rules:"
+	RulesDisabled []string // "Disabled rules:"
+	RulesAlways   []string // "Rules firing always:"
+	RulesReplica  []string // "Rules firing on replica only:"
+
+	TriggersEnabled  []string // "Triggers:"
+	TriggersDisabled []string // "Disabled triggers:"
+	TriggersAlways   []string // "Triggers firing always:"
+	TriggersReplica  []string // "Triggers firing on replica only:"
+
+	PartitionOf          []string // "Partition of:"
+	PartitionConstraints []string // "Partition constraint:"
+	PartitionKey         *string  // "Partition key:"
+	Partitions           []string // "Partitions:" (or leave empty)
+	PartitionsSummary    *string  // "Number of partitions ..." (non-verbose form)
+
+	Inherits           []string // "Inherits"
+	ChildTables        []string // "Child tables" (verbose)
+	ChildTablesSummary *string  // "Number of child tables..."
+	TypedTableOf       *string  // "Typed table of type:"
+	HasOIDs            *bool    // "Has OIDs: yes|no"
+	Options            *string  // "Options: ..."
+	Server             *string  // "Server: ..."  (foreign tables)
+	FDWOptions         *string  // "FDW Options: (...)" (foreign tables)
+	OwnedBy            *string  // "Owned by:" (sequences)
+}
+
+// DescribeTableResult holds the result of a describe table command.
+type DescribeTableResult struct {
+	Columns       []string
+	Data          [][]string
+	TableMetaData TableFooterMeta
 }
