51570fe4ac6e431629b8b74294dcf372650e1324
Balaji01-4D <j.balaji2468@gmail.com>
2025-12-13 13:47:15 +0530

    test: add test for list extensions

--- dbcommands/list_extensions_test.go
+++ dbcommands/list_extensions_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"testing"
 
+	"github.com/balaji01-4d/pgxspecial"
 	"github.com/balaji01-4d/pgxspecial/dbcommands"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/stretchr/testify/assert"
@@ -86,4 +87,38 @@ func TestListExtensionsWithPattern(t *testing.T) {
 	}
 	assert.Greater(t, len(allRows), 0, "Expected at least one extension")
 	assert.True(t, containsByField(allRows, "name", "plpgsql"), "Expected to find 'plpgsql' extension")	
+}
+
+
+func TestListExtensionsVerbose(t *testing.T) {
+
+	db := connectTestDB(t)
+	defer db.(*pgxpool.Pool).Close()
+	pattern := ""
+	verbose := true
+
+	res, err := dbcommands.ListExtensions(context.Background(), db, pattern, verbose)
+	if err != nil {
+		t.Fatalf("ListExtensions failed: %v", err)
+	}
+	if res.ResultKind() != pgxspecial.ResultKindExtensionVerbose {
+		t.Fatalf("Expected ResultKindExtensionVerbose, got %v", res.ResultKind())
+	}
+		result, ok := res.(pgxspecial.ExtensionVerboseListResult)
+		if !ok {
+			t.Fatalf("Expected ExtensionVerboseListResult, got %T", res)
+		}
+
+	assert.Greater(t, len(result.Results), 0, "Expected at least one extension")
+	found := false
+	for _, ext := range result.Results {
+		if ext.Name == "plpgsql" {
+			found = true
+			assert.Greater(t, len(ext.Description), 0, "Expected description for plpgsql extension")
+			assert.Contains(t, ext.Description, "function plpgsql_call_handler()", "Unexpected description content")
+			break
+		}
+	}
+
+	assert.True(t, found, "Expected to find 'plpgsql' extension")
 }
